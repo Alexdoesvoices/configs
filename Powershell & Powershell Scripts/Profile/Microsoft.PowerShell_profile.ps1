272a777bc29446d7ad7f2d1e709dc2704c7ef0e0
Alexdoesvoices <50270256+Alexdoesvoices@users.noreply.github.com>
2025-09-23 11:44:51 -0500

    updated

--- Powershell & Powershell Scripts/Profile/Microsoft.PowerShell_profile.ps1
+++ Powershell & Powershell Scripts/Profile/Microsoft.PowerShell_profile.ps1
@@ -1,34 +1,34 @@
-# Start starship dock:
-Invoke-Expression (&starship init powershell)
+# TODO: Uncomment the following line once starship is installed:
+# Invoke-Expression (&starship init powershell)
 
-function Pshelp-Container {   #! This is my 'help' command.
+function pshelp {   #? This is my 'help' command.
 @"
 cc - Clear the screen and Cat a file.
 clls - Clear the screen and list the contents of the directory at the same time.
 touch - Creates a file in the current directory.
 "@
 }
-Set-Alias pshelp Pshelp-Container
 
-function clear-cat {   #! This function clears the screen and cats a file at the same time.
+function clear-cat {   #? This function clears the screen and cats a file at the same time.
   Clear-Host
   Get-Content @args
 }
 Set-Alias cc clear-cat
 
-function clear-ls {   #! This function clears the screen and list out current directories content.
+function clear-ls {   #? This function clears the screen and list out current directories content.
     Clear-Host
     Get-ChildItem
 }
 Set-Alias clls clear-ls
 
-function touch($file) {   #! This function creates a file in the current directory with the provided name.
+function touch($file) {   #? This function creates a file in the current directory with the provided name.
         "" | Out-File $file -Encoding ASCII
 }
 
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
+# TODO: Uncomment the following 4 lines once Chocolaty is installed:
+# $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+# if (Test-Path($ChocolateyProfile)) {
+#   Import-Module "$ChocolateyProfile"
+# }
 
 Clear-Host
