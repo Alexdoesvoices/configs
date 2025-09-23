9a646f4c7abbfbc36f790936fa6e3b58110c315e
Alexdoesvoices <50270256+Alexdoesvoices@users.noreply.github.com>
2025-09-23 11:35:24 -0500

    updated the powershell profile to be more condensed and added the cc command.

--- Powershell & Powershell Scripts/Profile/Microsoft.PowerShell_profile.ps1
+++ Powershell & Powershell Scripts/Profile/Microsoft.PowerShell_profile.ps1
@@ -1,26 +1,28 @@
 # Start starship dock:
 Invoke-Expression (&starship init powershell)
 
-# PSHELP COMMAND
-function Pshelp-Container {
+function Pshelp-Container {   #! This is my 'help' command.
 @"
+cc - Clear the screen and Cat a file.
 clls - Clear the screen and list the contents of the directory at the same time.
 touch - Creates a file in the current directory.
 "@
 }
 Set-Alias pshelp Pshelp-Container
-# End.
 
-#! Alias for Clearing and Ls:
-function clear-ls {
+function clear-cat {   #! This function clears the screen and cats a file at the same time.
+  Clear-Host
+  Get-Content @args
+}
+Set-Alias cc clear-cat
+
+function clear-ls {   #! This function clears the screen and list out current directories content.
     Clear-Host
     Get-ChildItem
 }
 Set-Alias clls clear-ls
-# END
 
-#! Creates a file in the current directory:
-function touch($file) {
+function touch($file) {   #! This function creates a file in the current directory with the provided name.
         "" | Out-File $file -Encoding ASCII
 }
 
