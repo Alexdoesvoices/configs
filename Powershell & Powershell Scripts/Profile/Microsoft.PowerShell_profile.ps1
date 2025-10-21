5f439ccea10d1e29b5e66b4ca4cb2425b308a7e9
Alexdoesvoices <50270256+Alexdoesvoices@users.noreply.github.com>
2025-10-21 17:58:06 -0500

    Implement Compare-Cat function and update pshelp
    Added Compare-Cat function to compare file contents and enhanced pshelp command.

--- Powershell & Powershell Scripts/Profile/Microsoft.PowerShell_profile.ps1
+++ Powershell & Powershell Scripts/Profile/Microsoft.PowerShell_profile.ps1
@@ -4,6 +4,7 @@
 function pshelp {   #? This is my 'help' command.
 @"
 cc - Clear the screen and Cat a file.
+coc - Added Compare Cat.
 clls - Clear the screen and list the contents of the directory at the same time.
 touch - Creates a file in the current directory.
 "@
@@ -15,6 +16,33 @@ function clear-cat {   #? This function clears the screen and cats a file at the
 }
 Set-Alias cc clear-cat
 
+function Compare-Cat {
+    param(
+        # The file paths parameter now accepts an array of strings (string[])
+        [Parameter(Mandatory=$true, ValueFromRemainingArguments=$true)]
+        [string[]]$Paths 
+    )
+    
+    $SeparatorLine = "-----------------------------------"
+    Clear-Host 
+    
+    foreach ($Path in $Paths) {
+        try {
+            $FileInfo = Get-Item -Path $Path -ErrorAction Stop
+            Write-Host ""
+            Write-Host "File: $($FileInfo.Name)" -ForegroundColor Green
+            Write-Host $SeparatorLine -ForegroundColor DarkGray
+            Get-Content -Path $FileInfo.FullName
+            Write-Host $SeparatorLine -ForegroundColor DarkGray
+            
+        } catch {
+            Write-Error "Failed to process file '$Path'. Detail: $($_.Exception.Message)"
+            continue 
+        }
+    }
+}
+set-alias coc Compare-Cat;
+
 function clear-ls {   #? This function clears the screen and list out current directories content.
     Clear-Host
     Get-ChildItem
@@ -24,6 +52,7 @@ Set-Alias clls clear-ls
 function touch($file) {   #? This function creates a file in the current directory with the provided name.
         "" | Out-File $file -Encoding ASCII
 }
+
 
 # TODO: Uncomment the following 4 lines once Chocolaty is installed:
 # $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
