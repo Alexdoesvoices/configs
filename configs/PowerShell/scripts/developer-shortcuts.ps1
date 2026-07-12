e043048667c0270e1924ec7e3f9a0109d8fd3a69
Alexander Robinson <50270256+Alexdoesvoices@users.noreply.github.com>
2026-07-12 16:32:13 -0500

    updated copies

--- configs/PowerShell/scripts/developer-shortcuts.ps1
+++ configs/PowerShell/scripts/developer-shortcuts.ps1
@@ -3,3 +3,22 @@ function openZED-openPNPMdev {
     pnpm run dev --open
 }
 Set-Alias zpdo openZED-openPNPMdev
+
+
+function chezmoi-all-in-one {
+    # -join ' ' takes all arguments passed to the function and merges them into one string
+    $message = $args -join ' '
+
+    if (-not $message) { $message = "Update dotfiles" }
+
+    chezmoi re-add
+    chezmoi git -- add .
+    chezmoi git -- commit -m "$message"
+    chezmoi git -- push
+}
+Set-Alias cm-push chezmoi-all-in-one
+
+# Chezmoi shortcut!
+function cm {
+    chezmoi @args
+}
